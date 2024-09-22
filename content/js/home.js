ec2022f8eac25e10f59f93ed7fe1285359850a26
Emilio Bensignor <liobensignor@gmail.com>
2024-09-22 16:19:45 -0300

    Swiper en Home

--- content/js/home.js
+++ content/js/home.js
@@ -2,121 +2,186 @@ const logoSlider = document.getElementById("logoSlider");
 
 const logosClientes = [
   {
-    img: "abasto.png",
-    alt: "Logo Abasto",
-    nombre: "Abasto",
+    img: "ajinomoto.webp",
+    alt: "Ajinomoto",
+    nombre: "Ajinomoto",
   },
   {
-    img: "bangho.jpg",
+    img: "abasto.webp",
+    alt: "Logo Abasto",
+    nombre: "Abasto Shopping",
+  },
+  {
+    img: "bangho.webp",
     alt: "Logo Bangho",
     nombre: "Bangho",
   },
   {
-    img: "bayer.svg",
+    img: "barbieri.webp",
+    alt: "Barbieri",
+    nombre: "Barbieri",
+  },
+  {
+    img: "bayer.webp",
     alt: "Logo Bayer",
     nombre: "Bayer",
   },
   {
-    img: "danica.png",
+    img: "danica.webp",
     alt: "Logo Danica",
-    nombre: "Danica",
+    nombre: "Dánica",
   },
   {
-    img: "disney.svg",
+    img: "disney.webp",
     alt: "Logo Disney",
     nombre: "Disney",
   },
   {
-    img: "fuca.svg",
-    alt: "Logo Fuca",
-    nombre: "Fuca",
+    img: "fam.webp",
+    alt: "FAM",
+    nombre: "Fundación Argentina de Mieloma",
   },
   {
-    img: "garbarino.svg",
+    img: "fox.webp",
+    alt: "FOX",
+    nombre: "FOX Channel",
+  },
+  {
+    img: "fruttami.webp",
+    alt: "Fruttami",
+    nombre: "Frutta-mi",
+  },
+  {
+    img: "fuca.webp",
+    alt: "Logo Fuca",
+    nombre: "FUCA",
+  },
+  {
+    img: "garbarino.webp",
     alt: "Logo Garbarino",
     nombre: "Garbarino",
   },
   {
-    img: "irsa.png",
+    img: "irsa.webp",
     alt: "Logo Irsa",
-    nombre: "Irsa",
+    nombre: "IRSA",
   },
   {
-    img: "klaukol.svg",
+    img: "klaukol.webp",
     alt: "Logo Klaukol",
     nombre: "Klaukol",
   },
   {
-    img: "megatlon.png",
+    img: "megatlon.webp",
     alt: "Logo Megatlon",
     nombre: "Megatlon",
   },
   {
-    img: "melia.svg",
+    img: "melia.webp",
     alt: "Logo Melia",
-    nombre: "Melia",
+    nombre: "Meliá Hotels International",
   },
   {
-    img: "msc.png",
+    img: "motoroma.webp",
+    alt: "Motoroma",
+    nombre: "Moto Roma",
+  },
+  {
+    img: "msc.webp",
     alt: "Logo Msc",
-    nombre: "Msc",
+    nombre: "MSC Cruceros",
   },
   {
-    img: "parex.svg",
+    img: "parex.webp",
     alt: "Logo Parex",
     nombre: "Parex",
   },
   {
-    img: "paty.svg",
+    img: "paty.webp",
     alt: "Logo Paty",
     nombre: "Paty",
   },
   {
-    img: "qualy.svg",
+    img: "qualy.webp",
     alt: "Logo Qualy",
     nombre: "Qualy",
   },
   {
-    img: "quickfood.png",
+    img: "quickfood.webp",
     alt: "Logo Quickfood",
     nombre: "Quickfood",
   },
   {
-    img: "revear.svg",
+    img: "revear.webp",
     alt: "Logo Revear",
     nombre: "Revear",
   },
   {
-    img: "sadia.svg",
+    img: "sadia.webp",
     alt: "Logo Sadia",
     nombre: "Sadia",
   },
   {
-    img: "vie-tur.svg",
+    img: "sika.webp",
+    alt: "Sika",
+    nombre: "Sika",
+  },
+  {
+    img: "sodimac.webp",
+    alt: "Sodimac",
+    nombre: "Sodimac",
+  },
+  {
+    img: "starbucks.webp",
+    alt: "Starbucks",
+    nombre: "Starbucks",
+  },
+  {
+    img: "vasser.webp",
+    alt: "Vasser",
+    nombre: "Vasser",
+  },
+  {
+    img: "vietur.webp",
     alt: "Logo Vietur",
     nombre: "Vietur",
   },
 ];
-const logosDuplicados = [...logosClientes, ...logosClientes, ...logosClientes];
 
-for (let logo = 0; logo < logosDuplicados.length; logo++) {
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.querySelector('.swiperLogos')) {
+    const swiperLogos = new Swiper('.swiperLogos', {
+      slidesPerView: 3,
+      loop: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      spaceBetween: 16,
+      breakpoints: {
+        480: {
+          slidesPerView: 4,
+        },
+        700: {
+          slidesPerView: 4,
+          spaceBetween: 32,
+        },
+        992: {
+          slidesPerView: 5,
+        },
+        1280: {
+          slidesPerView: 6,
+        },
+      }
+    })
+  }
+});
+
+for (let logo = 0; logo < logosClientes.length; logo++) {
   logoSlider.innerHTML += `
-    <div class="logoSlide wow fadeInUp">
-      <img src="/content/img/${logosDuplicados[logo].img}" alt="${logosDuplicados[logo].alt}" />
-      <p>${logosDuplicados[logo].nombre}</p>
+    <div class="swiper-slide wow fadeInUp">
+      <img src="/content/img/home/clientes/${logosClientes[logo].img}" alt="${logosClientes[logo].alt}" />
+      <p>${logosClientes[logo].nombre}</p>
     </div>
   `;
 }
-
-$("#logoSlider").slick({
-  infinite: true,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: false,
-  autoplay: true,
-  autoplaySpeed: 1000,
-  speed: 500,
-  swipeToSlide: false,
-  variableWidth: false,
-});
